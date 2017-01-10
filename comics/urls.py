3a925cb892b800a5c7958de6b9735b4b49f5891f
beewizzard <superbutters@gmail.com>
2017-01-10 16:11:50 -0500

    Added login workflow, redirects to tracker index on successful login

--- comics/urls.py
+++ comics/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+# from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^ComicCollectionTracker/', include('ComicCollectionTracker.urls')),
     url(r'^tracker/', include('ComicCollectionTracker.urls')),  # simpler alias
     url(r'^admin/', admin.site.urls),
+    url('^', include('django.contrib.auth.urls'))
 ]
