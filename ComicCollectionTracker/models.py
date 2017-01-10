99bcf7e80ec6bc766287fec8f5310dcd0b76a3fe
beewizzard <superbutters@gmail.com>
2017-01-10 14:52:29 -0500

    Changed model to use Django's User model

--- ComicCollectionTracker/models.py
+++ ComicCollectionTracker/models.py
@@ -1,11 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
-
-class User(models.Model):
-    email = models.CharField(max_length=200)
-    name = models.CharField(max_length=100)
+from django.contrib.auth.models import User
 
 
 class Collection(models.Model):
--- ComicCollectionTracker/views.py
+++ ComicCollectionTracker/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import User, Collection
+
 
 def index(request):
-    return HttpResponse("Welcome to your Comic Collection Tracker!")
+    collection_list = Collection.objects.all()  # TODO: replace with real query
+    context = {
+        'collection_list': collection_list,
+    }
+    if not collection_list:
+        return render(request, 'ComicCollectionTracker/create_collection_form.html', context)
